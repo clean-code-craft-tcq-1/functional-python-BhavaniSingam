2baec2cbd4361e81083ecb3cb4275044de1179a1
BhavaniSingam <39860323+BhavaniSingam@users.noreply.github.com>
2021-03-03 11:37:42 +0530

    Update BMSUnitTests.py

--- BMSUnitTests.py
+++ BMSUnitTests.py
@@ -2,7 +2,7 @@ from BatteryConditionChecks import isBatteryOk
 if __name__ == '__main__':
             assert(isBatteryOk(25, 70, 0.7) == True)
             assert(isBatteryOk(45, 80, 0.8) == True)
-            assert(isBatteryOk(50 , 90, 0.9) == False)
-            assert(isBatteryOk(25 , 15, 0.9) == False)
-            assert(isBatteryOk(25 , 65, 0.9) == False)
+            assert(isBatteryOk(50 , 95, 0.9) == False)
+            assert(isBatteryOk(23 , 10, 0.9) == False)
+            assert(isBatteryOk(24 , 65, 0.9) == False)
             assert(isBatteryOk(15, 85, 0.7) == False)
